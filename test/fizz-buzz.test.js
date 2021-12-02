b69241ac60dd38c83ff7811eee698ab7bf5f35cd
Samuel Maciel <ssmaciel_rp@hotmail.com>
2021-12-02 18:04:46 -0300

    testes

--- test/fizz-buzz.test.js
+++ test/fizz-buzz.test.js
@@ -1,3 +1,13 @@
+const { fizzBuzz } = require('../src/fizz-buzz')
+const { expect } = require('chai')
+
 describe('FizzBuzz', () => {
-    
+    it('Deve retornar Fizz, quando o numero for divisivel por 3', () => {
+        // Arrange
+        const numero = 6
+        // Act
+        const retorno = fizzBuzz(numero)
+        // Assert
+
+    })
 })

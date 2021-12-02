291c5348005628110ffd01219e4405f609b96a19
Pedro Haupenthal <pfh@vortx.com.br>
2021-12-02 18:22:28 -0300

    Update fizz-buzz.test.js

--- test/fizz-buzz.test.js
+++ test/fizz-buzz.test.js
@@ -2,21 +2,31 @@ const { fizzBuzz } = require('../src/fizz-buzz')
 const { expect } = require('chai')
 
 describe('FizzBuzz', () => {
-  it('Deve retornar Fizz, quando o numero for divisivel por 3', () => {
-    // Arrange
-    const numero = 6
-    // Act
-    const retorno = fizzBuzz(numero)
-    // Assert
-    expect(retorno).to.be.equal('Fizz')
-  })
+    it('Deve retornar Fizz, quando o numero for divisivel por 3', () => {
+        // Arrange
+        const numero = 6
+        // Act
+        const retorno = fizzBuzz(numero)
+        // Assert
+        expect(retorno).to.be.equal('Fizz')
+    })
 
-  it('Deve retornar Buzz, quando o número for divisível por 5', () => {
-    //Arrange
-    const numero = 10
-    // Act
-    const retorno = fizzBuzz(numero)
-    //Assert
-    expect(retorno).to.be.equal('Buzz')
-  })
+    it('Deve retornar Buzz, quando o número for divisível por 5', () => {
+        //Arrange
+        const numero = 10
+        // Act
+        const retorno = fizzBuzz(numero)
+        //Assert
+        expect(retorno).to.be.equal('Buzz')
+    })
+
+    it('Deve retornar FizzBuzz, quando o número for divisível por 3 e 5', () => {
+        //Arrange
+        const numero = 15
+        // Act
+        const retorno = fizzBuzz(numero)
+        //Assert
+        expect(retorno).to.be.equal('FizzBuzz')
+    })
 })
+ 

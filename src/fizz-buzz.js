9d3a928e4e33d93aab48758313e6b142126e89ee
Pedro Haupenthal <pfh@vortx.com.br>
2021-12-02 17:57:44 -0300

    add regra

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -1,4 +1,13 @@
 const fizzBuzz = (numero) => {
+  if (numero % 3 == 0 && numero % 5 == 0)
+    return 'FizzBuzz'
+
+  if (numero % 3 == 0)
+    return 'Fizz'
+
+  if (numero % 5 == 0)
+    return 'Buzz'
+
   return numero
 }
 

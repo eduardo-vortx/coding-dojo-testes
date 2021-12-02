dcfc04b6fe5ac240075857c3edc83b6207370cf7
Samuel Maciel <ssmaciel_rp@hotmail.com>
2021-12-02 18:29:33 -0300

    commit

--- test/fizz-buzz.test.js
+++ test/fizz-buzz.test.js
@@ -1,32 +1,40 @@
-const { fizzBuzz } = require('../src/fizz-buzz')
-const { expect } = require('chai')
+const { fizzBuzz } = require("../src/fizz-buzz");
+const { expect } = require("chai");
 
-describe('FizzBuzz', () => {
-    it('Deve retornar Fizz, quando o numero for divisivel por 3', () => {
-        // Arrange
-        const numero = 6
-        // Act
-        const retorno = fizzBuzz(numero)
-        // Assert
-        expect(retorno).to.be.equal('Fizz')
-    })
+describe("FizzBuzz", () => {
+  it("Deve retornar Fizz, quando o numero for divisivel por 3", () => {
+    // Arrange
+    const numero = 6;
+    // Act
+    const retorno = fizzBuzz(numero);
+    // Assert
+    expect(retorno).to.be.equal("Fizz");
+  });
 
-    it('Deve retornar Buzz, quando o número for divisível por 5', () => {
-        //Arrange
-        const numero = 10
-        // Act
-        const retorno = fizzBuzz(numero)
-        //Assert
-        expect(retorno).to.be.equal('Buzz')
-    })
+  it("Deve retornar Buzz, quando o número for divisível por 5", () => {
+    //Arrange
+    const numero = 10;
+    // Act
+    const retorno = fizzBuzz(numero);
+    //Assert
+    expect(retorno).to.be.equal("Buzz");
+  });
 
-    it('Deve retornar FizzBuzz, quando o número for divisível por 3 e 5', () => {
-        //Arrange
-        const numero = 15
-        // Act
-        const retorno = fizzBuzz(numero)
-        //Assert
-        expect(retorno).to.be.equal('FizzBuzz')
-    })
-})
- 
+  it("Deve retornar FizzBuzz, quando o número for divisível por 3 e 5", () => {
+    //Arrange
+    const numero = 15;
+    // Act
+    const retorno = fizzBuzz(numero);
+    //Assert
+    expect(retorno).to.be.equal("FizzBuzz");
+  });
+
+  it("Deve retornar o numero, quando o numero passado não for divisivel por 3 ou 5", () => {
+    //Arrange
+    const numero = 14;
+    // Act
+    const retorno = fizzBuzz(numero);
+    //Assert
+    expect(retorno).to.be.equal(numero);
+  });
+});
